47525ee820fefd76647c4b2a5b898a7098a85beb
avvida <avidakusuma9@gmail.com>
2025-10-14 11:42:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const tombolsapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
+                      
